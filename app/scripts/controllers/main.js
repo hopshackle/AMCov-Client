a08cfa35647019536815a67f9c79bd28fd7ce25d
hopshackle <james@janigo.co.uk>
2017-01-18 10:44:49 +0000

    Initial framework

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name amClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the amClientApp
+ */
+angular.module('amClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
