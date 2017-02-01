9b63e6970072e50f83cb894ba0e6d76ee7458277
hopshackle <james@janigo.co.uk>
2017-02-01 10:23:24 +0000

    Pull in raw data for magi seasons

--- app/scripts/controllers/covenant.js
+++ app/scripts/controllers/covenant.js
@@ -4,12 +4,26 @@ angular.module('amClientApp')
     .controller('CovCntrl', ['$routeParams', '$resource', function ($routeParams, $resource) {
         var cov = this;
         var baseURL = "http://localhost\:3000/api";
-        var db = $resource(baseURL + "/:covenant", null, {
+        var cov_db = $resource(baseURL + "/:covenant", null, {
             'update': { method: 'PUT' }
         });
-        db.get({ covenant: $routeParams.covenant }, function (covRecord) {
+        var season_db = $resource(baseURL + "/:covenant/:magus", null, {
+            'update': { method: 'PUT' }
+        });
+        cov_db.get({ covenant: $routeParams.covenant }, function (covRecord) {
             cov.covenantName = covRecord.name;
             cov.description = covRecord.description;
             cov.allMagi = covRecord.members;
+
+            for (var i in cov.allMagi) {
+                // get season data
+                var m = cov.allMagi[i];
+                season_db.query({ covenant: cov.covenantName, magus: m }, function (seasonData) {
+                    if (seasonData.length > 0) {
+                        var magus = seasonData[0].magus;
+                        cov[magus] = seasonData;
+                    }
+                });
+            }
         });
     }]);
